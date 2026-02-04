be468a3dba5703a58690702850d9943fa9b0fe72
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-02-04 10:56:18 +0530

    Time: 1 ms (75.24%) | Memory: 60.6 MB (43.00%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,18 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-     let index=0;
+    let index=0;
     for(let i=0;i<nums.length;i++){
-     
-      if(nums[i]!==0){
-        nums[index]=nums[i];
-        index++;
-      }
+        if(nums[i] !== 0){
+            nums[index] = nums[i];
+            index++;
+        }
     }
 
-    while(index<nums.length){
-        nums[index]=0;
-        index++
+    while(index < nums.length){
+        nums[index] = 0;
+        index++;
     }
- 
+    return nums
 };
