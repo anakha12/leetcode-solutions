5b6743b176fb0594d30844fac816e903119815d3
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-02-07 17:12:29 +0530

    Time: 3 ms (25.83%) | Memory: 63.4 MB (20.00%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.js
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.js
@@ -6,12 +6,10 @@
 var kLengthApart = function(nums, k) {
     let result=[]
     for(let i=0;i<nums.length;i++){
-        if(nums[i] == 1){
-            result.push(i)
-        }
+        if(nums[i] ==1) result.push(i)
     }
     for(let i=1;i<result.length;i++){
-        if(result[i] - result[i-1] <= k){
+        if(result[i] - result[i-1] <=k){
             return false
         }
     }
