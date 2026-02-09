6a218fc9848577c88b3ae57bf11b4d17070b849d
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-02-09 12:17:50 +0530

    Time: 14 ms (15.25%) | Memory: 56.6 MB (69.87%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    for(let i=0;i<arr.length;i++){
+       
+        if(arr[i]==0){
+            for(let j= arr.length-1;j >i ;j--){
+                arr[j] =arr[j-1]
+            }
+            if(i+1< arr.length){
+                arr[i+1] =0;
+            }
+            i++;
+        }
+        
+    }
+};
