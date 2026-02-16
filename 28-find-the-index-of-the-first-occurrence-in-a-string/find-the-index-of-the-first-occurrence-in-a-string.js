1ae84182121dd5175a2091156728a661a7b1a03d
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-02-16 12:28:02 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (75.10%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,15 +4,18 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if(haystack.length < needle.length) return -1;
-    for(let i=0;i<= haystack.length-needle.length;i++){
-        let j=0;
-        while(j< needle.length && haystack[i+j] === needle[j]){
-            j++
-        }
-        if(j === needle.length){
-            return i
-        }
-    }
-    return -1
+    // if(haystack.length < needle.length) return -1;
+    // for(let i=0;i<= haystack.length-needle.length;i++){
+    //     let j=0;
+    //     while(j< needle.length && haystack[i+j] === needle[j]){
+    //         j++
+    //     }
+    //     if(j === needle.length){
+    //         return i
+    //     }
+    // }
+    // return -1
+
+    return index=haystack.indexOf(needle) 
+    
 };
