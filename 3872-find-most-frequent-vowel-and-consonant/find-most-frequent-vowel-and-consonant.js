5b5ba23f905b765ff78aa1481ff6458e34f0b3ed
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-02-16 08:44:42 +0530

    Time: 9 ms (43.39%) | Memory: 58.2 MB (30.67%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 var maxFreqSum = function(s) {
-    let str = s.split("");
     let countV={}
     let countC={}
     let v="aeiou"
-    for(let i=0;i<str.length;i++){
-        if(v.includes(str[i])){
-           countV[str[i]] = (countV[str[i]] || 0)+1
+    for(let char of s){
+        if(v.includes(char)){
+           countV[char] = (countV[char] || 0)+1
         }else{
-            countC[str[i]] = (countC[str[i]] || 0)+1
+            countC[char] = (countC[char] || 0)+1
         }
     }
     let largestV = Math.max(...Object.values(countV),0);
