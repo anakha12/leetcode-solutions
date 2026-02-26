8252b5c8ee74b0adabd01d072658f8c823b00140
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-02-26 12:09:54 +0530

    Time: 8 ms (29.43%) | Memory: 60.8 MB (25.42%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+ let left =0;
+ let right=s.length-1;
+ while(left<right){
+    while(left< right && !isFollow(s[left])){
+        left++
+    }
+    while(left< right && !isFollow(s[right])){
+        right--
+    }
+   if(s[left].toLowerCase() !== s[right].toLowerCase()){
+    return false
+   }
+   left++;
+   right--
+ }
+ return true
+
+};
+function isFollow(char){
+    return /^[a-z0-9]$/i.test(char)
+}
