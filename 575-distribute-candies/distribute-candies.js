7afe47c1db9350bb7b208aa943b033817f9b4a38
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-03-16 17:31:54 +0530

    Time: 19 ms (92.42%) | Memory: 69.1 MB (29.21%) - LeetSync

--- 575-distribute-candies/distribute-candies.js
+++ 575-distribute-candies/distribute-candies.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var distributeCandies = function(candyType) {
-    let c=new Set(candyType);
-    if(candyType.length/2 < c.size)return candyType.length/2
-    return c.size;
+    let c=[...new Set(candyType)]
+    if(candyType.length/2 < c.length)return candyType.length/2
+    return c.length;
 };
