c8583b6e95f0f6e5894b10f0441f6ce476b8eb2c
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-05-07 15:42:37 +0530

    Time: 19 ms (73.58%) | Memory: 57.1 MB (41.81%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -3,23 +3,23 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    let longest=""
-   function expand(left,right){
-    while(left >=0 && right< s.length && s[left]==s[right]){
-        left--;
-        right++;
+    let longest="";
+    function expand(left, right){
+        while(left>=0 && right< s.length && s[left] ==s[right]){
+            left--;
+            right++
+        }
+        return s.slice(left+1,right)
     }
-     return s.slice(left+1, right)
-   }
-  for(let i=0;i<s.length;i++){
-    let odd= expand(i,i);
-    let even = expand(i,i+1);
-    if(odd.length>longest.length){
-        longest=odd;
+    for(let i=0;i<s.length;i++){
+        let odd=expand(i,i);
+        let even=expand(i,i+1);
+        if(odd.length > longest.length){
+            longest=odd;
+        }
+        if(even.length> longest.length){
+            longest=even
+        }
     }
-    if(even.length>longest.length){
-        longest=even
-    }
-  }
-  return longest
+    return longest
 };
