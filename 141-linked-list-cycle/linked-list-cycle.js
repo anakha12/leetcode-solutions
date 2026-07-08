ab04f0da55c078dbeb91b09c06fa39d96ee5a26f
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-07-08 17:43:58 +0530

    Time: 57 ms (43.13%) | Memory: 59.8 MB (19.11%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,15 +11,15 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+    if(!head) return false
     let slow=head;
-    let fast=head;
+    let fast=head.next;
     while(fast && fast.next){
-        slow=slow.next;
-        fast=fast.next.next;
         if(fast==slow){
             return true
         }
-        
+        slow=slow.next;
+        fast=fast.next.next;
 
     }
     return false
