952a8a4ed15ecb01f6807fc273ba08abb4e700eb
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-07-11 13:16:12 +0530

    Time: 50 ms (14.12%) | Memory: 53.9 MB (27.55%) - LeetSync

--- 278-first-bad-version/first-bad-version.js
+++ 278-first-bad-version/first-bad-version.js
@@ -21,16 +21,16 @@ var solution = function(isBadVersion) {
         let left=1;
         let right=n;
         let mid=0;
-    while(left<=right){
+    while(left<right){
         mid=left+Math.floor((right-left)/2);
         
         if(isBadVersion(mid)){
-            right=mid-1;
+            right=mid;
         }else{
             left=mid+1
         } 
     }
-    return left
+    return right
         
     };
 };
