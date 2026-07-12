d492594a0aa673f8e17831c26d0c596f4cc7ee2f
Anakha Santhosh <108932100+anakha12@users.noreply.github.com>
2026-07-12 21:02:31 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (81.60%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,11 +4,11 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let index =0;
+    let index=0;
     for(let i=0;i<nums.length;i++){
-        if(nums[i] !==val){
-            nums[index] = nums[i];
-            index++
+        if(nums[i]!==val){
+            nums[index]=nums[i]
+            index++;
         }
     }
     return index
